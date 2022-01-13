307737893c9a2577a851d98a2b091170abfbaa4e
ESKhatirMo <MohammedAmine.Khatir@es.nestle.com>
2022-01-13 17:56:00 +0100

    creation of project

---
+++ src/main/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+          Creation of project
+      </div>
+    )
+  }
+}
+
+export default App
